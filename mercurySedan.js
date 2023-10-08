47a3102900130565fb9326d83c925e9f655f44b9
Carlos Sosa <isek7or@gmail.com>
2023-10-08 00:44:30 -0700

    assignment completed

--- mercurySedan.js
+++ mercurySedan.js
@@ -10,13 +10,62 @@ console.log(v.make)
 
 //Note: You can code your derived Car class here or make a file named index.js and do it there.
 
-
 //TO DO: Code the Car subclass here or in index.js file, i.e. class Car extends Vehicle ...
 
+class Car extends VehicleModule {
+    constructor(make, model, year, color, mileage) {
+        super(make, model, year, color, mileage)
+        this.maximumPassengers = 5;
+        this.passengers = 0;
+        this.numberOfWheels = 4;
+        this.maximumSpeed = 160;
+        this.fuel = 10;
+        this.scheduleService = false;
+    }
+}
+
+loadPassenger(num) {
+    if (this.passenger < this.maximumPassengers) {
+        if ((num + this.passenger) <= this.maximumPassengers) {
+            this.passenger = num;
+            return this.passenger;
+        } else {
+            console.log(this.model + " " + this.make + " has exceeded its max passenger capacity");
+        }
+    } else {
+        console.log(this.model + " " + this.make + "has reached its max passenger capacity")
+    }
+}
+
+start() {
+    if (this.fuel > 0) {
+        console.log("engine started...!!!");
+        return this.started = true;
+    } else {
+        console.log("out of gas...");
+        return this.started = false;
+    }
+}
+
+checkService(mileage) {
+    if (this.mileage > 30000) {
+        console.log("time for maintenance")
+        return this.scheduleService = true;
+    }
+}
 
 
 
 
+
+let myCar = new Car('mercury', 'rad_sedan', '2002', 'white', 50000)
+
+myCar.start()
+myCar.loadPassenger(5)
+myCar.stop()
+myCar.checkService()
+
+console.log(myCar)
 
 
 
